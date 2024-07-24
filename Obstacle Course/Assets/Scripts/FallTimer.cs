8a10ccb397adcaaeb2255c9fe95fc6b35230873c
tomamac <mek777@hotmail.co.th>
2024-07-24 12:18:58 +0700

    Finished Obstacle Course

--- Obstacle Course/Assets/Scripts/FallTimer.cs
+++ Obstacle Course/Assets/Scripts/FallTimer.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class FallTimer : MonoBehaviour
 {
     [SerializeField]float timeToWait = 3f;
-    MeshRenderer renderer;
-    Rigidbody rigidbody;
+    MeshRenderer Mrenderer;
+    Rigidbody rigbod;
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<MeshRenderer>();
-        rigidbody = GetComponent<Rigidbody>();
-        renderer.enabled = false;
-        rigidbody.useGravity = false;
+        Mrenderer = GetComponent<MeshRenderer>();
+        rigbod = GetComponent<Rigidbody>();
+        Mrenderer.enabled = false;
+        rigbod.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log((int)Time.time);
+        // Debug.Log((int)Time.time);
         if(Time.time >= timeToWait)
         {
-            renderer.enabled = true;
-            rigidbody.useGravity = true;
+            Mrenderer.enabled = true;
+            rigbod.useGravity = true;
         }
     }
 }
